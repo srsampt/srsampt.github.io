bb11ef8476400a1395a168f070cb6334cc1451b2
srsampt <srsampt@gmail.com>
2025-10-17 19:04:39 -0400

    Update copyright
    using JS to get the current year

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+const copyrightSymbol = "\u00A9";
+const copyrightMessage = `Copyright ${copyrightSymbol} ${currentYear}, Sarah Sampt`
+
+document.getElementById('copyright').textContent = copyrightMessage
